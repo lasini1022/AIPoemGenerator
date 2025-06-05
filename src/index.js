4214cf2e58c0a932c1bb310795ae25f0585f40d9
lasini1022 <102490645+lasini1022@users.noreply.github.com>
2025-06-05 17:20:40 -0400

    added basic form handling anf typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "poem is added here",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemElement = document.querySelector("#poem-generator");
+poemElement.addEventListener("submit", generatePoem);
